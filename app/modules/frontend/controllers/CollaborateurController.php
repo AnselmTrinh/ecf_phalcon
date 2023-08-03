dd646968a7c968e27c6c914c9aee8ad3b2ba77e9
Votre Nom <Vous@exemple.com>
2023-08-04 03:42:34 +1100

    first commit

--- app/modules/frontend/controllers/CollaborateurController.php
+++ app/modules/frontend/controllers/CollaborateurController.php
@@ -4,34 +4,41 @@ declare(strict_types=1);
 namespace ecf_phalcon\modules\frontend\controllers;
 
 use Ecf_phalcon\Models\Collaborateur;
+use Phalcon\Http\Request;
 
 class CollaborateurController extends \Phalcon\Mvc\Controller
 {
-
     public function indexAction()
     {
         $collaborateursHtml = "
 <table class='table'>
-<thead>
-<tr>
-<th scope='col'>ID</th>
-<th scope='col'>Nom et Prénom</th>
-<th scope='col'>Niveau de compétence</th>
-<th scope='col'>Prime d'embauche</th>
-</tr>
-</thead>
-<tbody>";
+    <thead>
+    <tr>
+        <th scope='col'>ID</th>
+        <th scope='col'>Nom et Prénom</th>
+        <th scope='col'>Niveau de compétence</th>
+        <th scope='col'>Prime d'embauche</th>
+        <th scope='col'>Actions</th>
+    </tr>
+    </thead>
+    <tbody>";
 
-        foreach (Collaborateur::find() as $collaborateur) {
+        // Récupérer les données des collaborateurs depuis la table "collaborateur"
+        $collaborateurs = Collaborateur::find();
+
+        foreach ($collaborateurs as $collaborateur) {
             $collaborateursHtml .= "<tr>";
             $collaborateursHtml .= "<td>" . $collaborateur->getId() . "</td>";
             $collaborateursHtml .= "<td>" . $collaborateur->getNameLastname() . "</td>";
             $collaborateursHtml .= "<td>" . $collaborateur->getNiveauCompetence() . "</td>";
             $collaborateursHtml .= "<td>" . $collaborateur->getPrimeEmbauche() . "</td>";
+            $collaborateursHtml .= "<td>
+        </td>";
             $collaborateursHtml .= "</tr>";
         }
 
         $collaborateursHtml .= "</tbody></table>";
-        $this->view->setVar('collaborateursHtml', $collaborateursHtml);
+
+        $this->view->collab = $collaborateursHtml;
     }
 }
